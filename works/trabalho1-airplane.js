c24ab2f3c4f985fd454d9cd4325a29b37f0f55d7
raihlima <38303006+raihlima@users.noreply.github.com>
2021-06-14 21:26:42 -0300

    Avião no código

--- works/trabalho1-airplane.js
+++ works/trabalho1-airplane.js
@@ -1,54 +1,51 @@
-import * as THREE from          '../build/three.module.js';
-import Stats from               '../build/jsm/libs/stats.module.js';
-import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
-import {GUI} from               '../build/jsm/libs/dat.gui.module.js';
+import * as THREE from  '../build/three.module.js';
+import KeyboardState from '../libs/util/KeyboardState.js';
 import {initRenderer, 
-        initCamera,
-        InfoBox,
+        createGroundPlaneWired,
+        onWindowResize, 
         degreesToRadians,
         initDefaultBasicLight,
-        initDefaultSpotlight,
-        onWindowResize} from "../libs/util/util.js";
+        InfoBox} from "../libs/util/util.js";
+//import * as airplane from 'trabalho1-airplane.js'; // Import the airplane to this file here // TODO create cronstructor and so on to import correctly
 
-var stats = new Stats();          // To show FPS information
-var scene = new THREE.Scene();    // Create main scene
-var renderer = initRenderer();    // View function in util/utils
-var camera = initCamera(new THREE.Vector3(0, -30, 15)); // Init camera in this position
-initDefaultBasicLight(scene, 1, new THREE.Vector3(0, 0, 15)); // Adds some light to the scene
-//initDefaultSpotlight(scene, new THREE.Vector3(0.0, 0.0, 25.0)); // Adds a spotlight to the scene
+var scene = new THREE.Scene(); //create scene
+initDefaultBasicLight(scene, 1, new THREE.Vector3(0, 0, 25)); // Adds some light to the scene
 
-// Enable mouse rotation, pan, zoom etc.
-var trackballControls = new TrackballControls( camera, renderer.domElement );
+// airplane config
+var planePositionX = 0.0;
+var planePositionY = 20.0;
+var planePositionZ = 5.0;
 
-// FPS panel config
-function createStats() {
-  stats.setMode(0);
-  
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.left = '0';
-  stats.domElement.style.top = '0';
+var fuselageMaterial = new THREE.MeshPhongMaterial({color:"grey"});
+var mockPlaneGeometry = new THREE.BoxGeometry(9.5, 1.5, 1.5, 32);
+var mockPlane = new THREE.Mesh(mockPlaneGeometry, fuselageMaterial);
+mockPlane.position.set(planePositionX, planePositionY, planePositionZ); // initial position
+scene.add(mockPlane);
 
-  return stats;
-}
-// To show FPS
-stats = createStats();
-document.body.appendChild( stats.domElement );
+// Base mock sphere
+var mockBaseSphereGeometry = new THREE.SphereGeometry(1.01, 2, 2);
+var mockBaseSphere = new THREE.Mesh( mockBaseSphereGeometry, fuselageMaterial );
+// Set initial position of the sphere
+mockBaseSphere.translateX(0.0).translateY(-25.0).translateZ(0.0); // distance between airplane and camera
 
-// Show axes (parameter is size of each axis)
-var axesHelper = new THREE.AxesHelper( 12 );
-scene.add( axesHelper );
-// TODO move axes helper to the side
+// Camera configs
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var cameraPosition = [0,10,20];
+camera.position.set(cameraPosition[0], cameraPosition[1], cameraPosition[2]); // Initial camera position
+//camera.lookAt(0, 0, 0); // Set look at origin
+//camera.up.set(0, 1, 0);
 
-// create the ground plane
-var planeGeometry = new THREE.PlaneGeometry(25, 25);
-planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
-var planeMaterial = new THREE.MeshBasicMaterial({
-    color: "rgba(150, 150, 150)", // light grey
-    side: THREE.DoubleSide,
-});
-var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-// add the ground plane to the scene
-scene.add(plane);
+// Config camera holder
+var cameraHolder = new THREE.Object3D();
+cameraHolder.position.set(0.0, 2.0, 0.0);
+cameraHolder.up.set(0, 1, 0);
+cameraHolder.lookAt(0, 0, 0);
+//scene.add(cameraHolder);
+//mockPlane.add(cameraHolder);
+mockBaseSphere.add(cameraHolder);
+mockPlane.add(mockBaseSphere);
+cameraHolder.add(camera);
+//scene.add(mockBaseSphere);
 
 // define objects material
 var material = new THREE.MeshNormalMaterial();
@@ -305,13 +302,233 @@ baseCylinder.add(backLeftTire);
 baseCylinder.add(backRightTire);
 
 // add all objects to the scene
-scene.add(baseCylinder); // adds the whole airplane to the scene
+mockPlane.add(baseCylinder);
 
+
+var renderer = initRenderer();    // View function in util/utils
+
+// To use the keyboard
+var keyboard = new KeyboardState();
+
+// Show axes (parameter is size of each axis)
+var axesHelper = new THREE.AxesHelper( 15 );
+// Reposition of helper to better visualization of it
+//axesHelper.translateZ(-2);
+axesHelper.translateY(20); // TODO remove translation from axes helper
+axesHelper.translateX(2);
+//scene.add( axesHelper );
+
+var groundPlaneWired = createGroundPlaneWired(500, 500, 20, 20, "blue");
+//groundPlaneWired.translateY(0);
+groundPlaneWired.rotateX(degreesToRadians(90));
+scene.add(groundPlaneWired);
+groundPlaneWired.add(axesHelper);
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+// Adds light
+scene.add(new THREE.HemisphereLight());
+
+// Show text information onscreen
+showInformation(); // displays controls
+
+function airplaneSpeed(){
+    var speed = 0.2;
+
+    var controls = new function ()
+    {
+      this.onChangeAnimation = function(){
+        animationOn = !animationOn;
+      };
+      this.speed = baseAnimationSpeed;
+  
+      this.changeSpeed = function(){
+        speed = (this.speed);
+      };
+    }
+
+    cameraHolder.translateZ(-speed);
+}
+
+var speed = 0.2;
+var savedSpeed = speed;
+var pressed = [false,false]; //x,y
+var anglesVet = [0,0,0];
+
+var pivot = new THREE.Object3D();
+
+// Keyboard controls for cameraHolder
+function keyboardUpdateHolder() {
+
+    //
+    keyboard.update();
+    var angle = degreesToRadians(1);
+    var camX = new THREE.Vector3(1, 0, 0); // Set X axis
+    var camY = new THREE.Vector3(0, 1, 0); // Set Y axis
+    var camZ = new THREE.Vector3(0, 0, 1); // Set Z axis
+    //var x;
+    var mat4 = new THREE.Matrix4();
+    //mockBaseSphere.matrixAutoUpdate = false;
+    //mockBaseSphere.matrix.multiply(mat4.makeTranslation(speed, speed, speed))
+    //mockBaseSphere.matrix.multiply(mat4.makeRotationZ(angle)); // R1
+    //mockBaseSphere.matrix.multiply(mat4.makeTranslation(0.0, 1.0, 0.0));
+    
+    if (keyboard.pressed("left")){
+        mockPlane.rotateOnAxis(camZ, angle);
+       
+    }
+    if (keyboard.pressed("right")){
+        mockPlane.rotateOnAxis(camZ, -angle);
+    }
+    if (keyboard.pressed("up")){
+        pressed[0] = true;
+        //mockBaseSphere.rotateOnAxis(new THREE.Vector3(1, 0, 0), -angle);
+        //mockBaseSphere.matrix.multiply(mat4.makeRotationy(angle));
+        //mockBaseSphere.matrix.multiply(mat4.makeTranslation(0.0, 0.0, 0.0));
+        //mockBaseSphere.translateX(0).translateY(-25.0).translateZ(0);
+        //cameraHolder.rotateOnAxis(camX, -angle);
+        //mockBaseSphere.position.set(planePositionX,planePositionY,planePositionZ);
+        //mockBaseSphere.translateX(planePositionX).translateY(planePositionY-25.0).translateZ(planePositionZ);
+        //mockBaseSphere.matrix.multiply(mat4.makeRotationX(angle));
+        mockPlane.rotateOnAxis(camX, -angle);
+        anglesVet[0] = anglesVet[0] - angle;
+
+    }
+    if (keyboard.pressed("down")){
+        pressed[0] = true;
+        //mockBaseSphere.rotateOnAxis(camX, -angle);
+        mockPlane.rotateOnAxis(camX, angle);
+        //mockBaseSphere.matrix.multiply(mat4.makeRotationX(-angle));
+        //mockBaseSphere.rotateOnAxis(camX, -angle);
+        anglesVet[0] = anglesVet[0] + angle;
+    } 
+    //camera rotation // TODO adjust all other controls
+    //if (keyboard.pressed("<")) x = mockPlane.rotateOnAxis(camY, -angle);
+    //if (keyboard.pressed(">")) mockPlane.rotateOnAxis(camY, angle);
+    if (keyboard.pressed(",")){ // keep camera steady
+        mockBaseSphere.rotateOnAxis(camY, +angle);
+        mockPlane.rotateOnAxis(camY, -angle);
+        pressed[1] = true;
+        anglesVet[1] = anglesVet[1] + angle;
+    }
+    if (keyboard.pressed(".")){
+        mockBaseSphere.rotateOnAxis(camY, -angle);
+        //mockBaseSphere.matrix.multiply(mat4.makeRotationY(-angle));
+        mockPlane.rotateOnAxis(camY, +angle);
+        pressed[1] = true;
+        anglesVet[1] = anglesVet[1] - angle;
+    }
+    
+    //if (keyboard.pressed("space")) cameraHolder.translateZ(-speed); // movement
+    //if (keyboard.pressed("R")) cameraHolder.translateZ(0.2); // reverse
+    if (keyboard.pressed("Q")){ // speed up
+        if (speed >= 1.0){ // set maximum speed
+            speed = 1.0;
+        } else {
+            speed += 0.01;
+        }
+        //console.log(speed);
+    } 
+    if (keyboard.pressed("A")){ // slow down
+        if (speed <= 0.0){ // set minimum speed
+            speed = 0.0;
+        } else {
+            speed -= 0.01;
+        }
+        //console.log(speed);
+    }
+    if (keyboard.down("space")){ // inspection mode switch
+        if (speed > 0.0){
+        //    savedSpeed = speed;
+        }
+        if(groundPlaneWired.visible == false){
+            groundPlaneWired.visible = true;
+            speed = savedSpeed;
+        } else { 
+            groundPlaneWired.visible = false;
+            savedSpeed = speed;
+            speed = 0.0;
+        }
+        /*if(groundPlaneWired.visible == false){
+            groundPlaneWired.visible = true;
+        } else { 
+            groundPlaneWired.visible = false;
+        }*/
+        //console.log(speed); TODO solve bug at zero speed, change minimum speed to above 0.0???
+    }
+
+    // Controles não precionados
+    if (keyboard.up(",")){ // keep camera steady
+        //mockBaseSphere.rotateOnAxis(camY, angle);
+        //mockPlane.rotateOnAxis(camY, -angle);
+        pressed[1] = false;
+    }
+    if (keyboard.up(".")){
+        //mockBaseSphere.rotateOnAxis(camY, -angle);
+        //mockPlane.rotateOnAxis(camY, angle);
+        pressed[1] = false;
+    }
+    if (keyboard.up("up")){ // keep camera steady
+        //mockBaseSphere.rotateOnAxis(camY, angle);
+        //mockPlane.rotateOnAxis(camY, -angle);
+        pressed[0] = false;
+    }
+    if (keyboard.up("down")){
+        //mockBaseSphere.rotateOnAxis(camY, -angle);
+        //mockPlane.rotateOnAxis(camY, angle);
+        pressed[0] = false;
+    }
+
+    if(!pressed[1]){
+        if(anglesVet[1]<0){
+            mockBaseSphere.rotateOnAxis(camY, angle);
+            mockPlane.rotateOnAxis(camY, -angle);
+            //pressed[1] = f;
+            anglesVet[1] = anglesVet[1] + angle;
+            
+            if(anglesVet[1]>=0){
+                anglesVet[1]=0;
+
+            }
+        } else if(anglesVet[1]>0){
+            mockBaseSphere.rotateOnAxis(camY, -angle);
+            mockPlane.rotateOnAxis(camY, +angle);
+            //pressed[1] = f;
+            anglesVet[1] = anglesVet[1] - angle;
+            /*
+            if(anglesVet[1]<=0){
+                anglesVet[1]=0;
+            }*/
+        }
+    }
+
+    if(!pressed[0]){
+        if(anglesVet[0]<0){
+            //mockBaseSphere.rotateOnAxis(camX, angle);
+            mockPlane.rotateOnAxis(camX, +angle);
+            //pressed[1] = f;
+            anglesVet[0] = anglesVet[0] + angle;
+            /*
+            if(anglesVet[0]>=0){
+                anglesVet[0]=0;
+            }*/
+        } else if(anglesVet[0]>0){
+            //mockBaseSphere.rotateOnAxis(camX, -angle);
+            mockPlane.rotateOnAxis(camX, -angle);
+            //pressed[1] = f;
+            anglesVet[0] = anglesVet[0] - angle;
+            /*
+            if(anglesVet[0]<=0){
+                anglesVet[0]=0;
+            }*/
+        }
+    }
+}
 
 // Set angles of rotation
-var angle = 0.0;
-var baseAnimationSpeed = 0.2; // defines base animation speed
-var speed = baseAnimationSpeed;
+//var baseAnimationSpeed = 0.2; // defines base animation speed
+//var speedBlade = baseAnimationSpeed;
 var animationOn = true; // control if animation is on or of
 
 function rotateBlades(){
@@ -325,7 +542,6 @@ function rotateBlades(){
 
   if(animationOn){
     // defines angular speed
-    angle+=speed;
     
     var mat4 = new THREE.Matrix4();
     //hub.matrix.identity();  // reset matrix
@@ -334,58 +550,36 @@ function rotateBlades(){
     rightBlade.matrix.identity();  // reset matrix
 
     // Will execute rotation
-    hub.matrix.multiply(mat4.makeRotationY(speed/2)); // R1
-    rightHub.matrix.multiply(mat4.makeRotationY(-speed)); // R1
-    leftHub.matrix.multiply(mat4.makeRotationY(speed)); // R1
+    hub.matrix.multiply(mat4.makeRotationY(speed*2/2)); // R1
+    rightHub.matrix.multiply(mat4.makeRotationY(-speed*2)); // R1
+    leftHub.matrix.multiply(mat4.makeRotationY(speed*2)); // R1
   }
 }
 
-// Use this to show information onscreen
-var controls = new InfoBox();
-  controls.add("Airplane Preview");
-  controls.addParagraph();
-  controls.add("Use mouse to interact:");
-  controls.add("* Left button to rotate");
-  controls.add("* Right button to translate (pan)");
-  controls.add("* Scroll to zoom in/out.");
-  controls.addParagraph();
-  controls.add("TIP: Use upper panel to control the animation");
-  controls.show();
 
-  // shows blade rotation speed control
-  function buildInterface()
-  {
-    var controls = new function ()
-    {
-      this.onChangeAnimation = function(){
-        animationOn = !animationOn;
-      };
-      this.speed = baseAnimationSpeed;
-  
-      this.changeSpeed = function(){
-        speed = (this.speed);
-      };
-    };
-  
-    // GUI interface
-    var gui = new GUI();
-    gui.add(controls, 'onChangeAnimation',true).name("Animation");
-    gui.add(controls, 'speed', 0.1, 1.0) // defines speeds intervals
-      .onChange(function(e) { controls.changeSpeed() })
-      .name("Speed");
-  }
-  
-  buildInterface();
-
-// Listen window size changes
-window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
-
-render();
-function render()
+function showInformation()
 {
-  stats.update(); // Update FPS
-  trackballControls.update(); // Enable mouse movements
-  requestAnimationFrame(render);
-  rotateBlades(); // Enable blades rotation
-  renderer.render(scene, camera) // Render scene
+  // Use this to show information onscreen
+    var controls = new InfoBox();
+    controls.add("Flight Simulator controls:");
+    controls.addParagraph();
+    controls.add("Press arrow keys to change movement direction");
+    controls.add("Press , (comma) or . (point) to change camera angle");
+    controls.add("Press SPACE to toggle inspection mode");
+    controls.add("Press Q to move faster");
+    controls.add("Press A to move slower");
+    //controls.add("Press R to reverse move");
+    //controls.add("Press A to toggle Axes Helper visibility");
+    controls.show();
 }
+
+function render() {
+    requestAnimationFrame( render );
+	renderer.render( scene, camera );
+    keyboardUpdateHolder(); // listens to keyboard inputs and controls cameraHolder
+    //cameraHolder.translateZ(-speed) // moves the camera automatically
+    mockPlane.translateY(speed); // moves the plane automatically
+    //mockBaseSphere.translateY(speed);
+    rotateBlades();
+}
+render();
