a528f4b4b3bbe124258558afed5dec0386b2f429
raihlima <38303006+raihlima@users.noreply.github.com>
2021-06-14 21:28:25 -0300

    Avião no trabalho

--- works/trabalho1-flight-simulator.js
+++ works/trabalho1-flight-simulator.js
@@ -30,7 +30,8 @@ mockBaseSphere.translateX(0.0).translateY(-25.0).translateZ(0.0); // distance be
 
 // Camera configs
 var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0.0, 10.0, 20.0); // Initial camera position
+var cameraPosition = [0,10,20];
+camera.position.set(cameraPosition[0], cameraPosition[1], cameraPosition[2]); // Initial camera position
 //camera.lookAt(0, 0, 0); // Set look at origin
 //camera.up.set(0, 1, 0);
 
@@ -40,9 +41,269 @@ cameraHolder.position.set(0.0, 2.0, 0.0);
 cameraHolder.up.set(0, 1, 0);
 cameraHolder.lookAt(0, 0, 0);
 //scene.add(cameraHolder);
+//mockPlane.add(cameraHolder);
 mockBaseSphere.add(cameraHolder);
 mockPlane.add(mockBaseSphere);
 cameraHolder.add(camera);
+//scene.add(mockBaseSphere);
+
+// define objects material
+var material = new THREE.MeshNormalMaterial();
+var fuselageMaterial = new THREE.MeshPhongMaterial({color:"grey"});
+var cockpitMaterial = new THREE.MeshPhongMaterial({color:"white"});
+var tailMaterial = new THREE.MeshPhongMaterial({color:"orange", emissive:"rgb(255, 100, 0)", emissiveIntensity:"0.75"}); // bright orange
+var tiresMaterial = new THREE.MeshPhongMaterial({color:"black"}); 
+var hubMaterial = new THREE.MeshPhongMaterial({color:"red"});
+var stabilizersMaterial = new THREE.MeshPhongMaterial({color:"blue"});
+var flapsMaterial = new THREE.MeshPhongMaterial({color:"yellow"});
+var lifesaverMaterial = new THREE.MeshPhongMaterial({color:"red", emissiveIntensity:"0.95"}); // bright red
+
+
+// Reference URL to all ariplane parts names
+// https://www.flyaeroguard.com/learning-center/parts-of-an-airplane/
+
+
+//-----------------------------------//
+// FUSELAGE                          //
+//-----------------------------------//
+// define airplane wings geometry
+var wingsGeometry = new THREE.BoxGeometry(10.0, 3.0, 0.2);
+// create the right wing
+var rightWing = new THREE.Mesh(wingsGeometry, fuselageMaterial);
+rightWing.position.set(6.5, -1.0, 0.0);
+// create the left wing
+var leftWing = new THREE.Mesh(wingsGeometry, fuselageMaterial);
+leftWing.position.set(-6.5, -1.0, 0.0);
+// wing engines
+var enginesCylinderGeometry = new THREE.CylinderGeometry(1.0, 1.0, 4.0, 32);
+// left engine
+var leftEngineCylinder = new THREE.Mesh(enginesCylinderGeometry, fuselageMaterial);
+leftEngineCylinder.position.set(0.0, 0.0, -0.5);
+// right engine
+var rightEngineCylinder = new THREE.Mesh(enginesCylinderGeometry, fuselageMaterial);
+rightEngineCylinder.position.set(0.0, 0.0, -0.5);
+
+// define airplane flaps geometry
+var flapsGeometry = new THREE.BoxGeometry(10.0, 0.4, 0.2);
+var leftFlap = new THREE.Mesh(flapsGeometry, flapsMaterial);
+var rightFlap = new THREE.Mesh(flapsGeometry, flapsMaterial);
+leftFlap.position.set(0.0, -1.72, 0.0);
+rightFlap.position.set(0.0, -1.72, 0.0);
+
+// create the base cylinder
+var baseCylinderGeometry = new THREE.CylinderGeometry(1.5, 1.5, 9.0, 32);
+var baseCylinder = new THREE.Mesh(baseCylinderGeometry, fuselageMaterial);
+baseCylinder.position.set(0.0, 0.0, 2.5);
+
+// create the rear cylinder
+var backCylinderGeometry = new THREE.CylinderGeometry(1.5, 0.5, 5, 32);
+var backCylinder = new THREE.Mesh(backCylinderGeometry, fuselageMaterial);
+backCylinder.position.set(0.0, -7.0, 0.0);
+
+// create tail cylinder
+var tailCylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
+var tailCylinder = new THREE.Mesh(tailCylinderGeometry, fuselageMaterial);
+tailCylinder.position.set(0.0, -3.0, 0.0);
+
+// define airplane stabilizers geometry
+var horizontalStabilizerGeometry = new THREE.BoxGeometry(3, 1, 0.2);
+var verticalStabilizerGeometry = new THREE.BoxGeometry(0.2, 0.5, 2);
+// create the right stabilizer
+var rightStabilizer = new THREE.Mesh(horizontalStabilizerGeometry, stabilizersMaterial);
+rightStabilizer.position.set(2.0, 0.0, 0.0);
+// create the left stabilizer
+var leftStabilizer = new THREE.Mesh(horizontalStabilizerGeometry, stabilizersMaterial);
+leftStabilizer.position.set(-2.0, 0.0, 0.0);
+// create vertical stabilizer
+var verticalStabilizer = new THREE.Mesh(verticalStabilizerGeometry, tailMaterial);
+verticalStabilizer.position.set(0.0, -0.25, 1.5);
+// define airplane back flaps geometry
+var backFlapsGeometry = new THREE.BoxGeometry(3.0, 0.4, 0.2);
+var backRudderGeometry = new THREE.BoxGeometry(0.2, 0.4, 2.0);
+var backLeftFlap = new THREE.Mesh(backFlapsGeometry, flapsMaterial);
+var backRightFlap = new THREE.Mesh(backFlapsGeometry, flapsMaterial);
+var backRudder = new THREE.Mesh(backRudderGeometry, flapsMaterial);
+backLeftFlap.position.set(0.0, -0.72, 0.0);
+backRightFlap.position.set(0.0, -0.72, 0.0);
+backRudder.position.set(0.0, -0.47, 0.0);
+
+
+// EASTER EGGs BEGIN //
+// life saver easter egg
+var rightLifesaverGeometry = new THREE.TorusGeometry(0.4, 0.2, 8, 24);
+var rightLifesaver = new THREE.Mesh(rightLifesaverGeometry, lifesaverMaterial);
+rightLifesaver.position.set(1.6, -3.5, 0.0);
+rightLifesaver.rotateX(degreesToRadians(90));
+rightLifesaver.rotateY(degreesToRadians(90));
+
+// fake radar
+var radarGeometry = new THREE.OctahedronGeometry(0.8, 3);
+var radar = new THREE.Mesh(radarGeometry, fuselageMaterial);
+radar.position.set(0.0, 0.0, -1.5);
+
+// define airplane 3D crosses geometry
+var crossGeometry = new THREE.BoxGeometry(1.0, 0.4, 0.4);
+// left
+var leftCrossp1 = new THREE.Mesh(crossGeometry, lifesaverMaterial); // cross part 1
+var leftCrossp2 = new THREE.Mesh(crossGeometry, lifesaverMaterial); // cross part 2
+leftCrossp1.rotateZ(degreesToRadians(90));
+// right
+var rightCrossp1 = new THREE.Mesh(crossGeometry, lifesaverMaterial); // cross part 1
+var rightCrossp2 = new THREE.Mesh(crossGeometry, lifesaverMaterial); // cross part 2
+rightCrossp1.rotateZ(degreesToRadians(90));
+// EASTER EGGs END //
+
+
+// create the front cylinder
+var frontCylinderGeometry = new THREE.CylinderGeometry(0.5, 1.5, 0.5, 32);
+var frontCylinder = new THREE.Mesh(frontCylinderGeometry, fuselageMaterial);
+frontCylinder.position.set(0.0, 4.75, 0.0);
+
+
+//-----------------------------------//
+// PROPELLER                         //
+//-----------------------------------//
+// create blades hub
+var hubGeometry = new THREE.ConeGeometry(0.5, 1.0, 32);
+var hub = new THREE.Mesh(hubGeometry, hubMaterial);
+// define blades geometry
+var bladeGeometry = new THREE.BoxGeometry(0.2, 0.05, 5.0);
+var wingsBladeGeometry = new THREE.BoxGeometry(0.2, 0.05, 4.0);
+// create blades
+var blade = new THREE.Mesh(bladeGeometry, cockpitMaterial);
+// adds blade to the hub
+hub.add(blade);
+// Base hub sphere
+var hubBaseSphereGeometry = new THREE.SphereGeometry(0.01, 2, 2);
+var hubBaseSphere = new THREE.Mesh( hubBaseSphereGeometry, material );
+// Set initial position of the sphere
+hubBaseSphere.translateX(0.0).translateY(0.75).translateZ(0.0);
+// adds the hub to the base sphere
+hubBaseSphere.add(hub);
+// engines on wings
+var leftHub = new THREE.Mesh(hubGeometry, hubMaterial);
+var leftBlade = new THREE.Mesh(wingsBladeGeometry, cockpitMaterial);
+var rightHub = new THREE.Mesh(hubGeometry, hubMaterial);
+var rightBlade = new THREE.Mesh(wingsBladeGeometry, cockpitMaterial);
+var leftHubBaseSphere = new THREE.Mesh( hubBaseSphereGeometry, material );
+var rightHubBaseSphere = new THREE.Mesh( hubBaseSphereGeometry, material );
+// Set initial position of the sphere
+leftHubBaseSphere.translateX(0.0).translateY(2.5).translateZ(0.0);
+rightHubBaseSphere.translateX(0.0).translateY(2.5).translateZ(0.0);
+// adds the hub to the base sphere
+leftHubBaseSphere.add(leftHub);
+rightHubBaseSphere.add(rightHub);
+// adds blades to the hubs
+leftHub.add(leftBlade);
+rightHub.add(rightBlade);
+
+
+//-----------------------------------//
+// LANDING GEAR                      //
+//-----------------------------------//
+// creates tires geometry
+var tiresGeometry = new THREE.TorusGeometry(0.2, 0.1, 8, 24);
+// front
+var frontTire = new THREE.Mesh(tiresGeometry, tiresMaterial);
+frontTire.position.set(0.0, 3.0, -2.2);
+// back left
+var backLeftTire = new THREE.Mesh(tiresGeometry, tiresMaterial);
+backLeftTire.position.set(-1.5, -3.0, -2.2);
+// back right
+var backRightTire = new THREE.Mesh(tiresGeometry, tiresMaterial);
+backRightTire.position.set(1.5, -3.0, -2.2);
+// rotate tires to adjust angles
+frontTire.rotateX(degreesToRadians(90));
+frontTire.rotateY(degreesToRadians(90));
+backLeftTire.rotateX(degreesToRadians(90));
+backLeftTire.rotateY(degreesToRadians(90));
+backRightTire.rotateX(degreesToRadians(90));
+backRightTire.rotateY(degreesToRadians(90));
+
+// create shock strut geometry
+var shockStrutGeometry = new THREE.BoxGeometry(0.05, 0.2, 0.85);
+var backShockStrutsGeometry = new THREE.BoxGeometry(0.05, 0.2, 1.1);
+// create front shock strut
+var shockStrut = new THREE.Mesh(shockStrutGeometry, fuselageMaterial);
+shockStrut.position.set(0.2, 3.0, -1.9);
+// create 2nd front shock strut
+var shockStrut2 = new THREE.Mesh(shockStrutGeometry, fuselageMaterial);
+shockStrut2.position.set(-0.2, 3.0, -1.9);
+// create front tire cylinder axis
+var frontTireCylinderGeometry = new THREE.CylinderGeometry(0.1, 0.1, 0.4, 32);
+var frontTireCylinder = new THREE.Mesh(frontTireCylinderGeometry, fuselageMaterial);
+frontTireCylinder.rotateZ(degreesToRadians(90));
+frontTireCylinder.position.set(0.0, 3.0, -2.2);
+// create back left shock strut
+var backLeftShockStrut = new THREE.Mesh(backShockStrutsGeometry, fuselageMaterial);
+backLeftShockStrut.position.set(-0.9, -3.0, -1.75);
+// rotate to 90° angle
+backLeftShockStrut.rotateY(degreesToRadians(45));
+// create back right shock strut
+var backRightShockStrut = new THREE.Mesh(backShockStrutsGeometry, fuselageMaterial);
+backRightShockStrut.position.set(0.9, -3.0, -1.75);
+// rotate to 90° angle
+backRightShockStrut.rotateY(degreesToRadians(-45));
+// create back tire cylinder axis
+var backTiresCylinderGeometry = new THREE.CylinderGeometry(0.1, 0.1, 3.0, 32);
+var backTiresCylinder = new THREE.Mesh(backTiresCylinderGeometry, fuselageMaterial);
+backTiresCylinder.rotateZ(degreesToRadians(90));
+backTiresCylinder.position.set(0.0, -3.0, -2.2);
+
+// create the pilot's cockpit
+var cockpitGeometry = new THREE.SphereGeometry(1, 32, 32);
+var cockpit = new THREE.Mesh(cockpitGeometry, cockpitMaterial);
+cockpit.position.set(0.0, -2.5, 1.25);
+
+
+// Joins every airplane part togheter
+// PROPELLER
+frontCylinder.add(hubBaseSphere);
+// FUSELAGE
+// main structure
+baseCylinder.add(frontCylinder);
+baseCylinder.add(backCylinder);
+baseCylinder.add(rightLifesaver);
+baseCylinder.add(radar);
+backCylinder.add(tailCylinder);
+// wings
+baseCylinder.add(leftWing);
+baseCylinder.add(rightWing);
+leftWing.add(leftEngineCylinder);
+rightWing.add(rightEngineCylinder);
+leftEngineCylinder.add(leftHubBaseSphere);
+rightEngineCylinder.add(rightHubBaseSphere);
+leftWing.add(leftFlap);
+rightWing.add(rightFlap);
+// cockpit
+baseCylinder.add(cockpit);
+// TAIL
+tailCylinder.add(leftStabilizer);
+tailCylinder.add(rightStabilizer);
+tailCylinder.add(verticalStabilizer);
+leftStabilizer.add(backLeftFlap);
+leftStabilizer.add(leftCrossp1);
+leftStabilizer.add(leftCrossp2);
+rightStabilizer.add(backRightFlap);
+rightStabilizer.add(rightCrossp1);
+rightStabilizer.add(rightCrossp2);
+verticalStabilizer.add(backRudder);
+// LANDING GEAR
+// front
+baseCylinder.add(shockStrut);
+baseCylinder.add(shockStrut2);
+baseCylinder.add(frontTireCylinder);
+baseCylinder.add(frontTire);
+// back
+baseCylinder.add(backLeftShockStrut);
+baseCylinder.add(backRightShockStrut);
+baseCylinder.add(backTiresCylinder);
+baseCylinder.add(backLeftTire);
+baseCylinder.add(backRightTire);
+
+// add all objects to the scene
+mockPlane.add(baseCylinder);
+
 
 var renderer = initRenderer();    // View function in util/utils
 
@@ -95,47 +356,68 @@ var savedSpeed = speed;
 var pressed = [false,false]; //x,y
 var anglesVet = [0,0,0];
 
+var pivot = new THREE.Object3D();
+
 // Keyboard controls for cameraHolder
 function keyboardUpdateHolder() {
 
+    //
     keyboard.update();
     var angle = degreesToRadians(1);
     var camX = new THREE.Vector3(1, 0, 0); // Set X axis
     var camY = new THREE.Vector3(0, 1, 0); // Set Y axis
     var camZ = new THREE.Vector3(0, 0, 1); // Set Z axis
-    var x;
+    //var x;
+    var mat4 = new THREE.Matrix4();
+    //mockBaseSphere.matrixAutoUpdate = false;
+    //mockBaseSphere.matrix.multiply(mat4.makeTranslation(speed, speed, speed))
+    //mockBaseSphere.matrix.multiply(mat4.makeRotationZ(angle)); // R1
+    //mockBaseSphere.matrix.multiply(mat4.makeTranslation(0.0, 1.0, 0.0));
     
     if (keyboard.pressed("left")){
         mockPlane.rotateOnAxis(camZ, angle);
+       
     }
     if (keyboard.pressed("right")){
         mockPlane.rotateOnAxis(camZ, -angle);
     }
     if (keyboard.pressed("up")){
         pressed[0] = true;
-        //mockBaseSphere.rotateOnAxis(camX, angle);
+        //mockBaseSphere.rotateOnAxis(new THREE.Vector3(1, 0, 0), -angle);
+        //mockBaseSphere.matrix.multiply(mat4.makeRotationy(angle));
+        //mockBaseSphere.matrix.multiply(mat4.makeTranslation(0.0, 0.0, 0.0));
+        //mockBaseSphere.translateX(0).translateY(-25.0).translateZ(0);
+        //cameraHolder.rotateOnAxis(camX, -angle);
+        //mockBaseSphere.position.set(planePositionX,planePositionY,planePositionZ);
+        //mockBaseSphere.translateX(planePositionX).translateY(planePositionY-25.0).translateZ(planePositionZ);
+        //mockBaseSphere.matrix.multiply(mat4.makeRotationX(angle));
         mockPlane.rotateOnAxis(camX, -angle);
         anglesVet[0] = anglesVet[0] - angle;
+
     }
     if (keyboard.pressed("down")){
         pressed[0] = true;
+        //mockBaseSphere.rotateOnAxis(camX, -angle);
         mockPlane.rotateOnAxis(camX, angle);
+        //mockBaseSphere.matrix.multiply(mat4.makeRotationX(-angle));
+        //mockBaseSphere.rotateOnAxis(camX, -angle);
         anglesVet[0] = anglesVet[0] + angle;
     } 
     //camera rotation // TODO adjust all other controls
     //if (keyboard.pressed("<")) x = mockPlane.rotateOnAxis(camY, -angle);
     //if (keyboard.pressed(">")) mockPlane.rotateOnAxis(camY, angle);
     if (keyboard.pressed(",")){ // keep camera steady
-        mockBaseSphere.rotateOnAxis(camY, -angle);
-        mockPlane.rotateOnAxis(camY, angle);
-        pressed[1] = true;
-        anglesVet[1] = anglesVet[1] - angle;
-    }
-    if (keyboard.pressed(".")){
-        mockBaseSphere.rotateOnAxis(camY, angle);
+        mockBaseSphere.rotateOnAxis(camY, +angle);
         mockPlane.rotateOnAxis(camY, -angle);
         pressed[1] = true;
         anglesVet[1] = anglesVet[1] + angle;
+    }
+    if (keyboard.pressed(".")){
+        mockBaseSphere.rotateOnAxis(camY, -angle);
+        //mockBaseSphere.matrix.multiply(mat4.makeRotationY(-angle));
+        mockPlane.rotateOnAxis(camY, +angle);
+        pressed[1] = true;
+        anglesVet[1] = anglesVet[1] - angle;
     }
     
     //if (keyboard.pressed("space")) cameraHolder.translateZ(-speed); // movement
@@ -204,17 +486,20 @@ function keyboardUpdateHolder() {
             mockPlane.rotateOnAxis(camY, -angle);
             //pressed[1] = f;
             anglesVet[1] = anglesVet[1] + angle;
+            
             if(anglesVet[1]>=0){
                 anglesVet[1]=0;
+
             }
         } else if(anglesVet[1]>0){
             mockBaseSphere.rotateOnAxis(camY, -angle);
             mockPlane.rotateOnAxis(camY, +angle);
             //pressed[1] = f;
             anglesVet[1] = anglesVet[1] - angle;
+            /*
             if(anglesVet[1]<=0){
                 anglesVet[1]=0;
-            }
+            }*/
         }
     }
 
@@ -224,21 +509,53 @@ function keyboardUpdateHolder() {
             mockPlane.rotateOnAxis(camX, +angle);
             //pressed[1] = f;
             anglesVet[0] = anglesVet[0] + angle;
+            /*
             if(anglesVet[0]>=0){
                 anglesVet[0]=0;
-            }
+            }*/
         } else if(anglesVet[0]>0){
             //mockBaseSphere.rotateOnAxis(camX, -angle);
             mockPlane.rotateOnAxis(camX, -angle);
             //pressed[1] = f;
             anglesVet[0] = anglesVet[0] - angle;
+            /*
             if(anglesVet[0]<=0){
                 anglesVet[0]=0;
-            }
+            }*/
         }
     }
-
 }
+
+// Set angles of rotation
+//var baseAnimationSpeed = 0.2; // defines base animation speed
+//var speedBlade = baseAnimationSpeed;
+var animationOn = true; // control if animation is on or of
+
+function rotateBlades(){
+  // takes back matrix control
+  hub.matrixAutoUpdate = false;
+  blade.matrixAutoUpdate = false;
+  leftHub.matrixAutoUpdate = false;
+  leftBlade.matrixAutoUpdate = false;
+  rightHub.matrixAutoUpdate = false;
+  rightBlade.matrixAutoUpdate = false;
+
+  if(animationOn){
+    // defines angular speed
+    
+    var mat4 = new THREE.Matrix4();
+    //hub.matrix.identity();  // reset matrix
+    blade.matrix.identity();  // reset matrix
+    leftBlade.matrix.identity();  // reset matrix
+    rightBlade.matrix.identity();  // reset matrix
+
+    // Will execute rotation
+    hub.matrix.multiply(mat4.makeRotationY(speed*2/2)); // R1
+    rightHub.matrix.multiply(mat4.makeRotationY(-speed*2)); // R1
+    leftHub.matrix.multiply(mat4.makeRotationY(speed*2)); // R1
+  }
+}
+
 
 function showInformation()
 {
@@ -262,5 +579,7 @@ function render() {
     keyboardUpdateHolder(); // listens to keyboard inputs and controls cameraHolder
     //cameraHolder.translateZ(-speed) // moves the camera automatically
     mockPlane.translateY(speed); // moves the plane automatically
+    //mockBaseSphere.translateY(speed);
+    rotateBlades();
 }
 render();
